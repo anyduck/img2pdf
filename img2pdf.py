69858e5322b769280db216300a6c76e2228c7ebb
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2021-02-28 18:04:22 +0200

    Add: auto portait orientation

--- img2pdf.py
+++ img2pdf.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 from pathlib import Path
+from PIL import Image
+
 
 SUFFIXES = (
     '.png',
@@ -7,10 +9,22 @@ SUFFIXES = (
     '.jpeg'
 )
 
-def get_images(folder: Path) -> list[Path]:
-    return [path for path in folder.iterdir() if path.is_file() and path.suffix in SUFFIXES]
+IS_AUTO_PORTAIT_MODE = True
+
+
+def get_images(folder: Path) -> list[Image.Image]:
+    return [Image.open(path) for path in folder.iterdir()
+            if path.is_file() and path.suffix in SUFFIXES]
+
+
+def convert2portrait(images: list[Image.Image]) -> None:
+    for image in images:
+        if image.width > image.height:
+            image.rotate(-90)  # TODO: detection of -90, 0, 90, 180
 
 
 if __name__ == '__main__':
-    folder = input('Enter folder: ')
-    print(get_images(Path(folder)))
+    folder = Path(input('Enter folder: '))
+    images = get_images(folder)
+    if IS_AUTO_PORTAIT_MODE:
+        convert2portrait(images)
