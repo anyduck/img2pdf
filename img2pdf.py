fcec6b4635975fd8e7f89ee84c357649613e84b1
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2021-02-28 17:17:39 +0200

    Add: retrieving images from a folder

--- img2pdf.py
+++ img2pdf.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
+from pathlib import Path
+
+SUFFIXES = (
+    '.png',
+    '.jpg',
+    '.jpeg'
+)
+
+def get_images(folder: Path) -> list[Path]:
+    return [path for path in folder.iterdir() if path.is_file() and path.suffix in SUFFIXES]
+
 
 if __name__ == '__main__':
-    pass
+    folder = input('Enter folder: ')
+    print(get_images(Path(folder)))
