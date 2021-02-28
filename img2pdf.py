592264bab6a94ba364157436cd59f3b5c9b86f85
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2021-02-28 19:16:00 +0200

    Add: quality option for saving

--- img2pdf.py
+++ img2pdf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from PIL import Image
-from argparse import ArgumentParser, Namespace
 
 
 SUFFIXES = (
@@ -22,28 +22,37 @@ def convert2portrait(images: list[Image.Image]) -> None:
             image.rotate(-90)  # TODO: detection of -90, 0, 90, 180
 
 
-def save2pdf(images: list[Image.Image], path: Path) -> None:
-    images[0].save(path, save_all=True, append_images=images[1:])
+def save2pdf(images: list[Image.Image], path: Path, quality: int) -> None:
+    images[0].save(path, format='pdf', quality=quality,
+                   save_all=True, append_images=images[1:])
 
 
-def main(folder: Path, output: Path, portrait: bool) -> None:
+def main(folder: Path, output: Path, quality: int, portrait: bool) -> None:
     images = get_images(folder)
     if portrait:
         convert2portrait(images)
-    save2pdf(images, output)
+    save2pdf(images, output, quality)
 
 
 def parse_args() -> Namespace:
     parser = ArgumentParser(description='Convert photos to pdf')
     parser.add_argument('folder', type=Path, help='folder with photos')
-    parser.add_argument('-o', '--output', type=Path, default='output.pdf',
-                        help='output pdf path')
-    parser.add_argument('-p', '--portrait', action='store_true',
-                        help='convert photos to portrait orientation')
+    parser.add_argument(
+        '-o', '--output', dest='output', type=Path, default='output.pdf',
+        help='output pdf path'
+    )
+    parser.add_argument(
+        '-q', '--quality', dest='quality', type=int, default=70,
+        help='quality of output images on a scale from 0 (worst) to 100 (best)'
+    )
+    parser.add_argument(
+        '-p', '--portrait', dest='portrait', action='store_true',
+        help='convert photos to portrait orientation'
+    )
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.folder, args.output, args.portrait)
+    main(args.folder, args.output, args.quality, args.portrait)
