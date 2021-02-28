c63635752e11bc587facee57fa426b5fd84c8cab
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2021-02-28 18:34:45 +0200

    Refactor: create main and parse_args functions

--- img2pdf.py
+++ img2pdf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from pathlib import Path
 from PIL import Image
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 
 SUFFIXES = (
@@ -26,16 +26,24 @@ def save2pdf(images: list[Image.Image], path: Path) -> None:
     images[0].save(path, save_all=True, append_images=images[1:])
 
 
-if __name__ == '__main__':
+def main(folder: Path, output: Path, portrait: bool) -> None:
+    images = get_images(folder)
+    if portrait:
+        convert2portrait(images)
+    save2pdf(images, output)
+
+
+def parse_args() -> Namespace:
     parser = ArgumentParser(description='Convert photos to pdf')
     parser.add_argument('folder', type=Path, help='folder with photos')
     parser.add_argument('-o', '--output', type=Path, default='output.pdf',
                         help='output pdf path')
     parser.add_argument('-p', '--portrait', action='store_true',
                         help='convert photos to portrait orientation')
-    args = parser.parse_args()
 
-    images = get_images(args.folder)
-    if args.portrait:
-        convert2portrait(images)
-    save2pdf(images, args.output)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.folder, args.output, args.portrait)
